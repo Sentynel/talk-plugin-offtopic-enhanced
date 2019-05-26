5cc9b0d805904e5d6833954ba390d8aa69ffe29a
Sam Lade <sam@sentynel.com>
2019-05-26 20:50:14 +0000

    Cleaner implementation of filter on topic
    Now with just one hack

--- client/components/OnTopicFilter.js
+++ client/components/OnTopicFilter.js
@@ -2,33 +2,32 @@ import React from 'react';
 import styles from './OnTopicFilter.css';
 import { t } from 'plugin-api/beta/client/services';
 
-// This is a horrifying hack - we fake a matching tag, which ensures that the
-// class will get applied to every comment.
-class FakeArray {
-  some = (x) => true;
+// This is a horrifying hack - the filter below that gets passed into addCommentClassName() will
+// ultimately have .some() called on the array to check for whether it contains the supplied tag
+// (this happens in the getConditionalClassNames() function).
+// There's no built-in support for the opposite operation, so we just construct an object which
+// lies about the result of the some() function.
+class LyingArray {
+  constructor(arr) {
+    this.arr = arr;
+  }
+  some = (x) => !this.arr.some(x);
 }
 
 export default class OnTopicFilter extends React.Component {
   tag = 'OFF_TOPIC';
-  classNameShow = 'talk-plugin-off-topic-comment-show';
-  classNameHide = 'talk-plugin-off-topic-comment-hide';
-  cnShow = { [this.classNameShow]: { tags: [this.tag] } };
-  cnHide = { [this.classNameHide]: { tags: new FakeArray() } };
+  className = 'talk-plugin-off-topic-comment-ontopic';
+  cn = { [this.className]: { tags: new LyingArray( [ this.tag ] ) } };
 
   handleChange = e => {
     if (e.target.checked) {
-      this.props.addCommentClassName(this.cnShow);
-      this.props.addCommentClassName(this.cnHide);
+      this.props.addCommentClassName(this.cn);
       this.props.toggleCheckboxOnTopic();
     } else {
-      const idxHide = this.props.commentClassNames.findIndex(
-        i => i[this.classNameHide]
+      const idx = this.props.commentClassNames.findIndex(
+        i => i[this.className]
       );
-      this.props.removeCommentClassName(idxHide);
-      const idxShow = this.props.commentClassNames.findIndex(
-        i => i[this.classNameShow]
-      );
-      this.props.removeCommentClassName(idxShow);
+      this.props.removeCommentClassName(idx);
       this.props.toggleCheckboxOnTopic();
     }
   };
